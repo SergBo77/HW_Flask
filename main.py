c2a20b828da33c27c72b880059630dd210f438bf
SergBo77 <seman77@mail.ru>
2024-06-23 21:20:24 +0300

    add all

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from flask import Flask, render_template
+import time
+import tkinter as tk
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def films():
+def start():
     return render_template("index.html")
 
 
-@app.route("/person/")
-def person():
-    return render_template("main.html")
+@app.route("/blog/")
+def blog():
+    return render_template("blog.html")
 
+@app.route("/contacts/")
+def contacts():
+    return render_template("contacts.html")
+
+@app.route("/time/")
+def time():
+    return render_template('time.html')
 
 if __name__ == "__main__":
     app.run()
